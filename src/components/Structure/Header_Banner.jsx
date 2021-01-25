de0d6e4f9fb7da6b693ab924edeead0beb1d8c70
poolpal <zoo5871@naver.com>
2021-01-25 22:53:13 +0900

    Change -Path of Image

--- src/components/Structure/Header_Banner.jsx
+++ src/components/Structure/Header_Banner.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react';
 import '../../design/css/Header.scss'
-import image from '../../../public/icons/logo.png'
 import {Button} from 'antd'
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
